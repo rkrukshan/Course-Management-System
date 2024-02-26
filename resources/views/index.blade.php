9712748722f0a1991b7c68e3781f09456c78ada1
Rukmanghan .S <rukshan1122@gmail.com>
2024-02-26 18:35:12 +0530

    ui alignment

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,7 +1,9 @@
 @extends('layout.front')
 @section('body')
 
-    
-
+<div class="container-fluid p-5 bg-primary text-white text-center">
+    <h1>Welcome</h1>
+    <p>Course Management System</p> 
+  </div>
     </html>
 @endsection
